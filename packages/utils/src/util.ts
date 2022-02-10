295daab51f16fe06022d574ff937604b66cdfdce
xywait <734445114@qq.com>
2022-02-10 11:26:35 +0800

    Signed-off-by: xywait <734445114@qq.com>

--- packages/utils/src/util.ts
+++ packages/utils/src/util.ts
@@ -87,3 +87,32 @@ export function getTimeNo(prefixes = '', suffixes= ''): string{
 			: millisecond;
 	return `${prefixes}${year}${month}${date}${hour}${minute}${second}${millisecond}${suffixes}`;
 };
+
+/**
+ * 判断是否为空
+ * @param val
+ * @returns
+ */
+export function validateNull(val):boolean {
+    if (val instanceof Date || typeof val === 'boolean' || typeof val === 'number') return false;
+    if (val instanceof Array) {
+        if (val.length === 0) return true;
+    } else if (val instanceof Object) {
+        for (var o in val) {
+            return false;
+        }
+        return true;
+    } else {
+        if (
+            val === 'null' ||
+            val == null ||
+            val === 'undefined' ||
+            val === undefined ||
+            val === ''
+        ) {
+            return true;
+        }
+        return false;
+    }
+    return false;
+}
